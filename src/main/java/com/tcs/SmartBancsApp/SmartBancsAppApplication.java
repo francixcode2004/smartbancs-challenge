46a469abef0cbd4d0428d97b4412a2bb71924f56
francisco lopez <franciscoricardo293@gmail.com>
2026-09-18 20:06:15 -0500

    Inicio Proyecto

---
+++ src/main/java/com/tcs/SmartBancsApp/SmartBancsAppApplication.java
@@ -0,0 +1,13 @@
+package com.tcs.SmartBancsApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBancsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartBancsAppApplication.class, args);
+	}
+
+}
